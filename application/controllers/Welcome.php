5764dc4e11952b2919bb39b717ad2d2b4774ccec
Anibal C <detops.tk@gmail.com>
2015-10-17 16:26:00 -0500

    configurar controller padre *config parent controller*

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends Public_Controller {
+
+	public function __construct() {
+		parent::__construct();
+		//$this->layout->setLayout('layouts/admin');
+	}
+
+	public function index()
+	{
+		$this->layout->setTitle("Home");
+		$this->layout->setKeywords("keywords");
+		$this->layout->setDescripcion("Descripción");
+		$this->layout->setSocialSiteName("Name");
+		$this->layout->setSocialTitle("Title");
+		$this->layout->setSocialResumen("Resumen");
+		$this->layout->setSocialDescripcion("Description");
+		//$this->layout->css( array('/assets/css/additional.css') );
+		//$this->layout->js( array('/assets/js/additional.js') );
+
+		$data["info"] = "Información";
+
+		//Layout load view
+		$this->layout->view('frontend/home/index', $data);
+	}
+}
