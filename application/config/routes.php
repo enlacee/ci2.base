b7dbb8abe615dc8a198ad665ba3d5d196ef5b905
Anibal C <detops.tk@gmail.com>
2015-11-06 12:40:28 -0500

    establecer codeigniter 3.0.x

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'home';
+$route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
@@ -76,4 +76,3 @@ $route['admin/agregar'] = 'backend/add';
 
 //Section edit (application/backend/edit.php)
 $route['admin/editar'] = 'backend/edit';
-
